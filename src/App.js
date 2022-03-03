551400aa4122e1e340a566ace94414e7d72898b0
Aski <aalto.vili.93@gmail.com>
2022-03-03 19:41:58 +0200

    Eikos taa ollu vaan materiaaleissa

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './App.css';
+import Todotable from './components/Todolist';
+
+function App() {
+  const [todo, setTodo] = useState({desc: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  return (
+    <div className="App">
+      <h1>X</h1>
+      <form onSubmit={addTodo}>
+        <input type="date" name="date" value={todo.date} onChange={inputChanged}/>
+        <input type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
+        <input type="submit" value="Add"/>
+      </form>
+      <Todotable todos={todos}/>
+    </div>
+  );
+}
+
+export default App;
